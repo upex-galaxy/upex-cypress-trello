db68c1dfceb44d1414a9696ccf5f280a9f921c8b
ZwitterKaneda <smentek@gmail.com>
2022-10-23 18:54:09 -0300

    GX-1913 Modificaciones de Archivos y Capertas

--- cypress/e2e/Tests/Suites/GX-1913/Stickers.AddStickerToCard.cy.js
+++ cypress/e2e/Tests/Suites/GX-1913/Stickers.AddStickerToCard.cy.js
@@ -1,20 +1,20 @@
-let trelloAPI
-let stickerList
-
-function getRandomStickers(min, max) {
-    let sticker1 = max - min + 1;
-    let sticker2 = Math.random() * sticker1;
-    let result = Math.floor(sticker2) + min;
-    return result;
-}
-
-let stickerNumber = getRandomStickers(1, 43);
-
-
-
 describe('✅{API} Trello | Stickers | API Endpoint: Add a Sticker to a Card', () => {
 
-    before('✅{API} Trello | Stickers', () => {
+    let trelloAPI
+    let stickerList
+
+    function getRandomStickers(min, max) {
+        let sticker1 = max - min + 1;
+        let sticker2 = Math.random() * sticker1;
+        let result = Math.floor(sticker2) + min;
+        return result;
+    }
+
+    let stickerNumber = getRandomStickers(1, 43);
+
+
+
+    before(() => {
 
         cy.fixture('DOM/GX-1913/trelloAPI.Page').then((MyApi) => { trelloAPI = MyApi });
         cy.fixture('DOM/GX-1913/stickersIDtrello.Page').then((Stick) => { stickerList = Stick });
@@ -103,7 +103,7 @@ describe('✅{API} Trello | Stickers | API Endpoint: Add a Sticker to a Card', (
                         cy.api({
                             method: 'POST',
                             url: `https://api.trello.com/1/cards/${IDnewcard}/stickers?image=63531133c44f770179beb787&top=${trelloAPI.top}&left=${trelloAPI.left}&zIndex=${trelloAPI.zIndex}&key=${trelloAPI.key}&token=${trelloAPI.token}`
-                            
+
                         }).then(({ body }) => {
                             const image = body.image
                             expect(image).equal(trelloAPI.customSticker)
