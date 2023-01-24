81a1aff82b92d31c3c2364494aa1c69ce26bfc23
Gabe <gabrielpaganelli96@gmail.com>
2023-01-24 14:51:38 -0300

    GX-3818 inicio de proyecto

---
+++ cypress/e2e/Tests/ComponentName/GX-3818-trelloCheckitems-Api.cy.js
@@ -0,0 +1,10 @@
+const key = "191a488f5f9b25380f53f34531eea047"
+const token = "03d4b617f0ed756a1e74de3856627c5dcba89a366f81cc04fee1e388df60f9a7"
+
+context('{API} Trello | Checkitems | API Endpoint: Create, Update and Delete Checkitems on Checklist', () => {
+
+    it('', () => {
+        
+    
+    });
+});
