16c6fa8c4f7da200684b13811f6d32dabb55ad49
dbquiroga <day@xcapit.com>
2023-06-13 17:35:38 -0300

    GX2-3889 first commit: change CI, add test, and test plan: sprint 19

---
+++ cypress/e2e/Tests/Trello-Cards/GX2-3889-cards.cy.js
@@ -0,0 +1,21 @@
+
+
+describe('US GX2-3889 | TS: Trello (API) | Cards | Crear, Modificar, Mover y Eliminar Tarjetas de un Tablero', () => {
+
+    before('', () => {
+        // se crea las listas "por hacer", "haciendo", "listo"
+    })
+    
+    it('3900 | TC1: Validar crear una card en lista "Por hacer”', () => {
+        
+    })
+    it('3901 | TC2: Validar modificar una card', () => {
+        
+    })
+    it('3902 | TC3:  Validar mover card de "Por hacer" a "Haciendo", a "Listo"', () => {
+        
+    })
+    it('3903 | TC4: Validar eliminar todas las cards en la lista "Listo"', () => {
+        
+    })
+}) 
