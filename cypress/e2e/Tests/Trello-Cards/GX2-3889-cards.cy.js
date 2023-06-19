445311b3ef9b8d0e70bc9c26faf27c74d9b82bd9
dbquiroga <day@xcapit.com>
2023-06-19 11:40:50 -0300

    GX2-3889 change way how to istance class

--- cypress/e2e/Tests/Trello-Cards/GX2-3889-cards.cy.js
+++ cypress/e2e/Tests/Trello-Cards/GX2-3889-cards.cy.js
@@ -1,68 +1,54 @@
-//import { apikey } from '../../fixtures/data/keysTrello.json';
-//import { secret } from '../../fixtures/data/keysTrello.json';
-
-import { cardsListsPage } from '@pages/cardsListsTrello.js';
+import { CardsListsTrello } from '@pages/cardsListsTrello.js';
 
 describe('US GX2-3889 | TS: Trello (API) | Cards | Crear, Modificar, Mover y Eliminar Tarjetas de un Tablero', () => {
-    const LISTO = "Listo"
-    const HACIENDO = "Haciendo"
-    const PORHACER = "Por Hacer"
+	const LISTO = 'Listo';
+	const HACIENDO = 'Haciendo';
+	const PORHACER = 'Por Hacer';
 
-	let dataKeys;
-    let listPages = {}
+	const cardsListsPage = new CardsListsTrello([LISTO, HACIENDO, PORHACER]);
+
+	let listPages = {};
+	let idCard;
 	before('', () => {
-		cy.fixture('/data/keysTrello').then(dato => {
-			dataKeys = dato;
+		// Crear las listas "por hacer", "haciendo", "listo"
+		cardsListsPage.createLists().then(resp => {
+			listPages = resp;
 		});
-
-		// 	// Crear las listas "por hacer", "haciendo", "listo"
-        cardsListsPage.createLists([ LISTO, HACIENDO, PORHACER ]).then(resp => {
-            listPages = resp
-        });
 	});
 
-	// after('Borrar las listas al correr los tests', () => {
-	// 	//borrar las listas despues de ejecutar los tests
-	// 	const listNames = ['Listo', 'Haciendo', 'Por Hacer'];
-	// 	listNames.forEach(listName => {
-	// 		cy.request({
-	// 			url: `https://api.trello.com/1/lists/${listId[listName]}?key=${dataKeys.apikey}&token=${dataKeys.token}`,
-	// 			method: 'DELETE',
-	// 		});
-	// 	});
-	// });
+	after('Borrar las listas al terminar de correr los tests', () => {
+		cardsListsPage.deleteAllLists().then(resp => {
+			expect(resp.status).to.be.equal(200);
+		});
+	});
 
 	it('3900 | TC1: Validar crear una card en lista "Por hacer”', () => {
-        createCard(listPages[PORHACER]).then(respuesta => {
+		cardsListsPage.createCard(listPages[PORHACER]).then(respuesta => {
+			cy.log(respuesta.body.id);
+			idCard = respuesta.body.id;
 			expect(respuesta.status).to.be.equal(200);
-			// expect(respuesta.body).to.have.keys('name'); / por alguna razon no funciona
+			// expect(respuesta.body).to.have.keys('name'); // por alguna razon no funciona
 		});
 	});
 
 	it('3901 | TC2: Validar modificar una card', () => {
-		cy.api({
-			method: 'PUT',
-			url: `https://api.trello.com/1/cards/${cardId}?key=${dataKeys.apiKey}&token=${dataKeys.token}`,
-			body: {
-				name: 'Una tarea muy importante por hacer',
-				desc: 'Primero saber los requerimientos',
-			}.then(respuesta => {
-				expect(respuesta.body).to.have.keys('cardId', 'apikey', 'token'); // Se hace asi esto?
-			}),
+		cardsListsPage.updateCard(idCard).then(resp => {
+			cy.log(idCard);
+
+			expect(idCard).to.exist;
+			expect(resp.status).to.be.equal(200);
+			//expect(resp.body).to.have.keys('cardId', 'apikey', 'token'); // Se hace asi esto?
 		});
 	});
 
 	it('3902 | TC3:  Validar mover card de "Por hacer" a "Haciendo", a "Listo"', () => {
-		cy.api({
-			method: 'PUT',
-			url: `https://api.trello.com/1/cards/${cardId}?idList=${listId}&key=${apiKey}&token=${token}`,
-			body: {},
+		cardsListsPage.moveList(idCard, HACIENDO).then(response => {
+			expect(response.status).to.be.equal(200);
 		});
 	});
-    it('3903 | TC4: Validar eliminar todas las cards en la lista "Listo"', () => {
-        cy.api({
-            method: 'DELETE',
-            url:,
-        })
-    });
+	it('3903 | TC4: Validar eliminar todas las cards en la lista "Listo"', () => {
+		cardsListsPage.deleteCards(listPages[LISTO]).then(response => {
+			expect(response.status).to.be.equal(200);
+		});
+	});
 });
--- cypress/support/pages/cardsListsTrello.js
+++ cypress/support/pages/cardsListsTrello.js
@@ -1,16 +1,29 @@
-import { resolve } from 'cypress/types/bluebird';
 import { apikey } from '../../fixtures/data/keysTrello.json';
 import { idBoard } from '../../fixtures/data/keysTrello.json';
 import { token } from '../../fixtures/data/keysTrello.json';
-import { idListToDo, idListDoing, idListDone } from '../../fixtures/data/keysTrello.json';
+export class CardsListsTrello {
+	constructor(listNames) {
+		this.ids = {}; // Propiedad para almacenar los IDs
+		this.listNames = listNames;
+	}
 
-class CardsListsTrello {
+	deleteAllLists() {
+		//borrar las listas despues de ejecutar los tests
+		return new Promise(resolve => {
+			cy.wrap(this.listNames).each(element => {
+				cy.request({
+					method: 'PUT',
+					url: `https://api.trello.com/1/lists/${element}/closed?key=${apikey}&token=${token}`,
+				}).then(response => {
+					resolve(response);
+				});
+			});
+		});
+	}
 	// Crear las listas "por hacer", "haciendo", "listo"
-	createLists(listNames) {
-		return new Promise((resolve, reject) => {
-			const ids = {}; // Objeto para almacenar los ids
-
-			cy.wrap(listNames)
+	createLists() {
+		return new Promise(resolve => {
+			cy.wrap(this.listNames)
 				.each(listName => {
 					cy.request({
 						url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${apikey}&token=${token}`,
@@ -19,27 +32,65 @@ class CardsListsTrello {
 							name: listName,
 						},
 					}).then(response => {
-						ids[listName] = response.body.id; // Asignar el id al objeto usando el nombre de la lista como clave
+						this.ids[listName] = response.body.id; // Asignar el id al objeto usando el nombre de la lista como clave
 					});
 				})
 				.then(() => {
-					resolve(ids);
+					resolve(this.ids);
 				});
 		});
 	}
 
-	createCard(idList) {
-		cy.request({
-			url: 'https://api.trello.com/1/cards',
-			method: 'POST',
-			body: {
-				name: 'Mi primer Card en Por Hacer',
-				idList: idList,
-				key: apikey,
-				token: token,
-			},
+	createCard(value) {
+		return new Promise(resolve => {
+			cy.request({
+				url: `https://api.trello.com/1/cards?idList=${value}&key=${apikey}&token=${token}`,
+				method: 'POST',
+				body: {
+					name: 'Mi primer Card en Por Hacer',
+				},
+			}).then(response => {
+				resolve(response);
+			});
+		});
+	}
+
+	updateCard(valueCard) {
+		return new Promise(resolve => {
+			cy.request({
+				method: 'PUT',
+				url: `https://api.trello.com/1/cards/${valueCard}?key=${apikey}&token=${token}`,
+				body: {
+					name: 'Una tarea muy importante por hacer',
+					desc: 'Primero saber los requerimientos',
+				},
+			}).then(response => {
+				resolve(response);
+			});
+		});
+	}
+
+	moveList(valueCard, listName) {
+		return new Promise(resolve => {
+			const idListValue = this.ids[listName];
+
+			cy.request({
+				method: 'PUT',
+				url: `https://api.trello.com/1/cards/${valueCard}?key=${apikey}&token=${token}&idList=${idListValue}`,
+			}).then(response => {
+				resolve(response);
+			});
+		});
+	}
+
+	deleteCards(list) {
+		return new Promise(resolve => {
+			cy.request({
+				method: 'POST',
+				url: `https://api.trello.com/1/lists/${list}/archiveAllCards?key=${apikey}&token=${token}`,
+			}).then(response => {
+				resolve(response);
+			});
 		});
 	}
 }
-
-export const cardsListsPage = new CardsListsTrello();
