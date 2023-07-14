8482117af61ec98c5537036f8cf7c63b0d959399
LucasSebastianCasco <lucassebaacasco@gmail.com>
2023-07-13 22:00:57 -0300

    GX2-4438 CardsTrello Test finalizado con exito

--- cypress/e2e/Tests/Trello-Cards/GX2-4438-CardsTrello.cy.js
+++ cypress/e2e/Tests/Trello-Cards/GX2-4438-CardsTrello.cy.js
@@ -3,24 +3,71 @@ removeLogs();
 import { cardsTrello } from '../../../support/pages/cardsPageTrello.Page.js';
 
 describe('GX2-4438Trello (API) | Cards | Crear, Modificar, Mover y Eliminar Tarjetas de un Tablero', () => {
-	//alta de listas
-	//before('', () => {});
-
-	// after('', () => {
-	// 	cardsTrello.deleteList();
-	// });
-
-	it.only('Prueba', () => {
-		//cy.log('hola');
-		cardsTrello.createList().then(response => {
+	let createListOne;
+	let createListTwo;
+	let createListThree;
+	let CardID;
+	before('', () => {
+		cardsTrello.createList('Done').then(response => {
 			expect(response.status).to.be.equal(200);
-			expect(response.body.name).to.be.equal('Lista 1');
+			expect(response.body.name).to.be.equal('Done');
+			createListOne = response;
+		});
+
+		cardsTrello.createList('Active').then(response => {
+			expect(response.status).to.be.equal(200);
+			expect(response.body.name).to.be.equal('Active');
+			createListTwo = response;
+		});
+
+		cardsTrello.createList('Backlog').then(response => {
+			expect(response.status).to.be.equal(200);
+			expect(response.body.name).to.be.equal('Backlog');
+			createListThree = response;
 		});
 	});
 
-	//it('alta de card', () => {});
+	after('Eliminar Listas', () => {
+		cardsTrello.deleteList(createListOne.body.id).then(responseOne => {
+			expect(responseOne.status).to.be.equal(200);
+		});
 
-	//it('update card', () => {});
+		cardsTrello.deleteList(createListTwo.body.id).then(responseTwo => {
+			expect(responseTwo.status).to.be.equal(200);
+		});
 
-	//it('mover card', () => {});
+		cardsTrello.deleteList(createListThree.body.id).then(responseThree => {
+			expect(responseThree.status).to.be.equal(200);
+		});
+	});
+
+	it('4439 | TC1: Create Card', () => {
+		let name = 'new task';
+		cardsTrello.createCard(createListThree.body.id, name).then(response => {
+			expect(response.status).to.be.equal(200);
+			expect(response.body.name).to.be.equal(name);
+			CardID = response.body.id;
+		});
+	});
+
+	it('4439 | TC2: Move card', () => {
+		cardsTrello.moveCard(CardID, createListTwo.body.id).then(response => {
+			expect(response.status).to.be.equal(200);
+			expect(response.body.idList).to.be.equal(createListTwo.body.id);
+		});
+	});
+
+	it('4439 | TC3: Edit card', () => {
+		let name = 'Task two';
+		cardsTrello.editCard(CardID, name).then(response => {
+			expect(response.status).to.be.equal(200);
+			expect(response.body.name).to.be.equal(name);
+		});
+	});
+
+	it('4439 | TC3: Delete card', () => {
+		cardsTrello.deleteCard(CardID).then(response => {
+			expect(response.status).to.be.equal(200);
+		});
+	});
 });
--- cypress/support/pages/cardsPageTrello.Page.js
+++ cypress/support/pages/cardsPageTrello.Page.js
@@ -7,32 +7,80 @@ class CardsTrello {
 		this.ids = {}; // Propiedad para almacenar los IDs
 		this.listNames = listNames;
 	}
-	createList() {
-		return cy.api({
-			url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${apikey}&token=${Token}`,
-
-			method: 'POST',
-			body: {
-				name: 'Lista 1',
-			}.then(response => {
-				return response;
-				//response.body.id;
-				//Cypress.env('listId', listId);
-				//this.ids[listName] = response.body.id;
-			}),
-		});
-	}
-
-	deleteList() {
+	createList(name) {
 		return cy
 			.api({
-				//url: `https://api.trello.com/1/lists/${this.listId}?key=${apikey}&token=${Token}`,
-				//url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${this.listId}&token=${Token}`,
-				url: `https://api.trello.com/1/lists/${Cypress.env('listId')}/closed?key=${apikey}&token=${Token}`,
+				url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${apikey}&token=${Token}`,
+
+				method: 'POST',
+				body: {
+					name: name,
+				},
+			})
+			.then(response => {
+				return response;
+			});
+	}
+
+	deleteList(idList) {
+		return cy
+			.api({
+				url: `https://api.trello.com/1/lists/${idList}/closed?key=${apikey}&token=${Token}&value=true`,
 				method: 'PUT',
 			})
 			.then(response => {
 				this.listId = null; // Reiniciar el ID de la lista en el constructor
+				return response;
+			});
+	}
+
+	createCard(idList, name) {
+		return cy
+			.api({
+				url: `https://api.trello.com/1/cards?idList=${idList}&key=${apikey}&token=${Token}`,
+				method: 'POST',
+				body: {
+					name: name,
+				},
+			})
+			.then(response => {
+				return response;
+			});
+	}
+
+	moveCard(idCard, idList) {
+		return cy
+			.api({
+				url: `https://api.trello.com/1/cards/${idCard}?key=${apikey}&token=${Token}&idList=${idList}`,
+				method: 'PUT',
+			})
+			.then(response => {
+				return response;
+			});
+	}
+
+	editCard(idCard, name) {
+		return cy
+			.api({
+				url: `https://api.trello.com/1/cards/${idCard}?key=${apikey}&token=${Token}`,
+
+				method: 'PUT',
+				body: {
+					name: name,
+				},
+			})
+			.then(response => {
+				return response;
+			});
+	}
+
+	deleteCard(idCard) {
+		return cy
+			.api({
+				url: `https://api.trello.com/1/cards/${idCard}?key=${apikey}&token=${Token}`,
+				method: 'DELETE',
+			})
+			.then(response => {
 				return response;
 			});
 	}
