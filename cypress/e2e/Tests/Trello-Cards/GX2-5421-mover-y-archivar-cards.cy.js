0a0aed34afc5c5a4746f0154efd275ad4da769c8
Pablo <pvertone390@gmail.com>
2023-08-10 21:10:36 -0300

    GX2-5421: Reoganizacion de archivos. Cambiando enfoque de pruebas, fijando datos de listas en un JSON

--- cypress/e2e/Tests/Trello-Cards/GX2-5421-mover-y-archivar-cards.cy.js
+++ cypress/e2e/Tests/Trello-Cards/GX2-5421-mover-y-archivar-cards.cy.js
@@ -1,16 +1,35 @@
-//import { trelloElement } from '@pages/GX2-5421/GX2-5421-create-lists-cards';
-//import { card } from '@pages/GX2-5421/GX2-5421-move-and-archive-cards';
+import { trello } from '@pages/GX2-5421-move-and-archive-cards';
+import { lists } from '../../fixtures/data/GX2-5421-mover-y-archivar-cards.json';
+let idListOne;
+let idListTwo;
 
 describe('US GX2-5421 | TS: Trello (API) | Cards | API Endpoint: mover y archivar todas las tarjetas de una lista', () => {
-	beforeEach('PRC: Usuario debe tener creadas dos listas y algunas cards', () => {
-		cy.get();
+	before('PRC: Usuario debe tener creadas dos listas y algunas cards', () => {
+		cy.viewport(1024, 768);
+		idListOne = lists.listOne.id;
+		idListTwo = lists.listTwo.id;
+
+		trello.createCard(idListOne, 'First Card').then(response => {
+			expect(response.status).to.eql(200);
+			expect(response.body.name).to.eql('First Card');
+		});
+
+		trello.createCard(idListOne, 'Second Card').then(response => {
+			expect(response.status).to.eql(200);
+			expect(response.body.name).to.eql('Second Card');
+		});
 	});
 
 	it('5422 | TC01: Validar poder mover todas las tarjetas de una lista a otra', () => {
-		cy.get();
+		trello.moveAllCards(idListOne, idListTwo).then(response => {
+			expect(response.status).to.eql(200);
+		});
+		//Cypress.env('idList');
 	});
 
 	it('5422 | TC02: Validar poder archivar todas las tarjetas de una lista', () => {
-		cy.get();
+		trello.archiveAllCards(idListTwo).then(response => {
+			expect(response.status).to.eql(200);
+		});
 	});
 });
---
+++ cypress/support/pages/GX2-5421-move-and-archive-cards.js
@@ -0,0 +1,66 @@
+import { global, board } from '../../fixtures/data/GX2-5421-mover-y-archivar-cards.json';
+
+class TrelloAPI {
+	createCard(idList, name) {
+		return cy
+			.api({
+				method: 'POST',
+				url: 'https://api.trello.com/1/cards',
+				qs: {
+					name: name,
+					idList: idList, //The ID of the list the card should be created in
+					key: global.key,
+					token: global.token,
+				},
+			})
+			.then(response => {
+				return response;
+			});
+	}
+
+	moveAllCards(idList, id) {
+		cy.api({
+			//Donde el param. id es el id de la lista en la que se encuentran las cards
+			method: 'POST',
+			url: `https://api.trello.com/1/lists/${id}/moveAllCards`,
+			qs: {
+				idBoard: board.id, //id del board al que deberían moverse las cards
+				idList: idList, //id de la lista a la que deberían moverse las cards
+				key: global.key,
+				token: global.token,
+			},
+		});
+		//NOTA: validar que la respuesta sea status 200
+	}
+
+	archiveAllCards(id) {
+		//requiere el ID (PP) de la lista donde están las cards
+		return cy
+			.api({
+				method: 'POST',
+				url: `https://api.trello.com/1/lists/${id}/archiveAllCards`,
+				qs: {
+					key: global.key,
+					token: global.token,
+				},
+			})
+			.then(response => {
+				return response;
+			});
+		//NOTA: validar que la respuesta sea status 200
+	}
+
+	archiveList(id) {
+		//id de la lista que va a ser eliminada
+		cy.api({
+			method: 'PUT',
+			url: `https://api.trello.com/1/lists/${id}/closed`,
+			qs: {
+				key: global.key,
+				token: global.token,
+			},
+		});
+	}
+}
+
+export const trello = new TrelloAPI();
