8af594fa2af082820418bbec9afbb25c3a8bf64e
igflores <139146133+igflores@users.noreply.github.com>
2023-11-22 00:44:04 -0800

    Test(GX2-10417): Updated the name of the TC01 On branch test/GX2-10417/CRUD-Sticker-on-a-Card Changes to be committed: 	modified:   cypress/e2e/Tests/Trello-Cards/GX2-10417-CRUD-Sticker-on-a-Card.cy.js

--- cypress/e2e/Tests/Trello-Cards/GX2-10417-CRUD-Sticker-on-a-Card.cy.js
+++ cypress/e2e/Tests/Trello-Cards/GX2-10417-CRUD-Sticker-on-a-Card.cy.js
@@ -1,13 +1,13 @@
 import { stickerCardPage } from '@pages/GX2-10417-CRUD-Sticker-on-a-Card-Page';
 
 describe('GX2-10417 - Trello (API) | Stickers | API Endpoint: Add, Update, Get, Delete a Sticker on a Card', () => {
-	it('Validate create a new card', () => {
+	it('Precondition: Create a new card', () => {
 		stickerCardPage.createCard().then(response => {
 			expect(response).to.exist.and.to.have.property('status', 200);
 		});
 	});
 
-	it('The card should contain a sticker', () => {
+	it('TC01: Should add a sticker on a card using valid values', () => {
 		stickerCardPage.addRandomStickerToCard().then(response => {
 			expect(response.status).to.be.equal(200);
 		});
