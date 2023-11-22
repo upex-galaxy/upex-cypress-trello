ebbd6103b5ed8538dc8f4d4ebf12665b0d542cf3
igflores <139146133+igflores@users.noreply.github.com>
2023-11-22 00:41:43 -0800

    Test(GX2-10417): (1)Added local variables to the POM archive, and the methods: createCard() and addRandomStickerToCard(). (2)Added the Precondition: Create a new card, and the TC01: Should add a sticker on a card using valid values. (3)Modified the fixture archive (GX2-10417-CRUDStickerOnACard.json) - minor changes

--- cypress/e2e/Tests/Trello-Cards/GX2-10417-CRUD-Sticker-on-a-Card.cy.js
+++ cypress/e2e/Tests/Trello-Cards/GX2-10417-CRUD-Sticker-on-a-Card.cy.js
@@ -1,1 +1,15 @@
-//Isabel test
+import { stickerCardPage } from '@pages/GX2-10417-CRUD-Sticker-on-a-Card-Page';
+
+describe('GX2-10417 - Trello (API) | Stickers | API Endpoint: Add, Update, Get, Delete a Sticker on a Card', () => {
+	it('Validate create a new card', () => {
+		stickerCardPage.createCard().then(response => {
+			expect(response).to.exist.and.to.have.property('status', 200);
+		});
+	});
+
+	it('The card should contain a sticker', () => {
+		stickerCardPage.addRandomStickerToCard().then(response => {
+			expect(response.status).to.be.equal(200);
+		});
+	});
+});
---
+++ cypress/support/pages/GX2-10417-CRUD-Sticker-on-a-Card-Page.js
@@ -0,0 +1,64 @@
+import { APIkey, APItoken, idList, arrayStickers } from 'cypress/fixtures/data/GX2-10417-CRUDStickerOnACard.json';
+
+const leftMin = -60;
+const leftMax = 100;
+const topMin = -60;
+const topMax = 100;
+const zIndexMin = 1;
+const zIndexMax = 5;
+const rotateMin = 0;
+const rotateMax = 360;
+const randomLeft = Math.floor(Math.random() * (leftMax - leftMin + 1) + leftMin);
+const randomTop = Math.floor(Math.random() * (topMax - topMin + 1) + topMin);
+const randomZIndex = Math.floor(Math.random() * (zIndexMax - zIndexMin + 1) + zIndexMin);
+const randomRotate = Math.floor(Math.random() * (rotateMax - rotateMin + 1) + rotateMin);
+const randomIndexSticker = Math.floor(Math.random() * arrayStickers.length);
+
+class StickerOnCardPage {
+	get() {
+		this.cardID = null;
+		this.stickerName = null;
+		this.stickerId = null;
+	}
+
+	createCard() {
+		return cy
+			.api({
+				method: 'POST',
+				url: '/cards',
+				qs: {
+					key: APIkey,
+					token: APItoken,
+					idList: idList,
+					name: 'CARD 1',
+					desc: 'probando 1 2 3',
+				},
+			})
+			.then(response => {
+				this.cardID = response.body.id;
+			});
+	}
+
+	addRandomStickerToCard() {
+		return cy
+			.api({
+				method: 'POST',
+				url: `/cards/${this.cardID}/stickers`,
+				qs: {
+					key: APIkey,
+					token: APItoken,
+					top: randomTop,
+					left: randomLeft,
+					zIndex: randomZIndex,
+					rotate: randomRotate,
+					image: arrayStickers[randomIndexSticker],
+				},
+			})
+			.then(response => {
+				this.stickerName = response.body.name;
+				this.stickerId = response.body.id;
+			});
+	}
+}
+
+export const stickerCardPage = new StickerOnCardPage();
