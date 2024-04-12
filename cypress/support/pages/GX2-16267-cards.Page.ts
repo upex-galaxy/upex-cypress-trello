8be629df56c0fa09e501fc13c5122be585810277
Marianela Serodino Runco <serodinonela@gmail.com>
2024-04-12 12:43:48 +0100

    GX3-3077 Deleted interface TrelloData from Cards.Page and moved it to ResponseType file

--- cypress/support/pages/GX2-16267-cards.Page.ts
+++ cypress/support/pages/GX2-16267-cards.Page.ts
@@ -1,11 +1,8 @@
-interface TrelloData {
-  key: string;
-  token: string;
-}
 import { processUrl } from '../../support/types/urlData';
 import type { urlList } from '../../support/types/urlData';
 import dataJson from '../../fixtures/data/GX2-16267-Cards.json';
-const data: TrelloData = dataJson as TrelloData;
+import type { TrelloDataPathParameters } from '../../support/types/responseType';
+const data: TrelloDataPathParameters = dataJson as TrelloDataPathParameters;
 
 export class TrelloCardApi {
 	static request(
@@ -14,7 +11,7 @@ export class TrelloCardApi {
 		id?: string,
 		body?: Record<string, any>
 	) {
-		const params = { idList: id, idCard: id, key: data.key, token: data.token };
+		const params = { idList: id, idCard: id, idSticker: id, image: data.image, left: data.left, top: data.top, zIndex: data.zIndex, key: data.key, token: data.token };
 		let url = processUrl(endpointKey, params);
 
 		return cy.api({
