e236bc17cac9f326af005887ea38d203be36cdc9
Marianela Serodino Runco <serodinonela@gmail.com>
2024-04-15 18:39:00 +0100

    GX3-3077 Added createList, deleteList  and GetAllStickersOnCard to urlList

--- cypress/support/types/urlData.ts
+++ cypress/support/types/urlData.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/naming-convention */
 export enum urlList {
+	createList = '/lists?name={name}&idBoard={idBoard}&key={key}&token={token}',
+	deleteList = '/lists/{idList}?key={key}&token={token}',
 	getList = '/lists/{idList}?key={key}&token={token}',
 	archiveCardsInList = '/lists/{idList}/archiveAllCards?key={key}&token={token}',
 	getCard = '/cards/{idCard}?key={key}&token={token}',
@@ -9,6 +11,7 @@ export enum urlList {
 	deleteCard = '/cards/{idCard}?&key={key}&token={token}',
 	addStickerToCard = '/cards/{idCard}/stickers?image={image}&top={top}&left={left}&zIndex={zIndex}&key={key}&token={token}',
 	getSticker = '/cards/{idCard}/stickers/{idSticker}?key={key}&token={token}',
+	getAllStckersOnCard = '/cards/{idCard}/stickers/?key={key}&token={token}'
 }
 export enum method {
 	GET = 'GET',
