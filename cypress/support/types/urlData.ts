8ec829d8d9b03d0ca9e15cae57b0d3725100895c
Marianela Serodino Runco <serodinonela@gmail.com>
2024-04-22 16:51:09 +0100

    GX3-3077 Changed url list order

--- cypress/support/types/urlData.ts
+++ cypress/support/types/urlData.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/naming-convention */
 export enum urlList {
-	createList = '/lists?name={name}&idBoard={idBoard}&key={key}&token={token}',
-	deleteList = '/lists/{idList}?key={key}&token={token}',
 	getList = '/lists/{idList}?key={key}&token={token}',
 	archiveCardsInList = '/lists/{idList}/archiveAllCards?key={key}&token={token}',
 	getCard = '/cards/{idCard}?key={key}&token={token}',
 	createCard = '/cards?idList={idList}&key={key}&token={token}',
 	updateCard = '/cards/{idCard}?&key={key}&token={token}',
 	deleteCard = '/cards/{idCard}?&key={key}&token={token}',
+	createList = '/lists?name={name}&idBoard={idBoard}&key={key}&token={token}',
+	deleteList = '/lists/{idList}?key={key}&token={token}',
 	addStickerToCard = '/cards/{idCard}/stickers?image={image}&top={top}&left={left}&zIndex={zIndex}&key={key}&token={token}',
 	getSticker = '/cards/{idCard}/stickers/{idSticker}?key={key}&token={token}',
 	updateSticker = '/cards/{idCard}/stickers/{idSticker}?top={top}&left={left}&zIndex={zIndex}&key={key}&token={token}',
