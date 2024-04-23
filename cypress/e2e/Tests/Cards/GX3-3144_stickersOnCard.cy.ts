1a53c84773c45dba160b5952b810c964dd8a4380
eleana2610 <eleanavcg@gmail.com>
2024-04-23 14:24:25 -0400

    update

--- cypress/e2e/Tests/Cards/GX3-3144_stickersOnCard.cy.ts
+++ cypress/e2e/Tests/Cards/GX3-3144_stickersOnCard.cy.ts
@@ -1,30 +1,43 @@
 import { apiSticker } from "@pages/APIStickers.page";
 import { addSticker } from "@pages/APIStickers.page";
+import { updateSticker } from "@pages/APIStickers.page";
+
 	const key = '60546f7606a6d9b65762f53b68d61aa2'
 	const token= 'ATTAba481ff908481346282087e391a6df7c1cad1c2f00b5baa233765cec3e5b26b26CBAC466'
 	const idList = '66228c917bf9a7fda9f9403d'
-	const idCard = '6626f9d5d127d0db725f4dfe'
-describe('Create Sticker on card',()=>{
-	it('Validate response', () => {
-		cy.request('https://trello.com/emoji').then(response => {
-			expect(response.status).to.eql(200);
-		
-		
-	})
-})
-});
+	const idCard = '6622c85faef1e64ad7dd77bb'
+	const idSticker= '6626f81286a6d105a50a6291'
 
-describe('Create a Card',()=>{
-	
-	it('create a card in Trello',() => {
+	describe('API Trello board ',()=>{
+	    it('TC01:create a card in Trello',() => {
 		apiSticker.createNewCard(idList,key,token,).then(response =>{
 			expect(response.status).to.eql(200);
 			expect(response.body.name).to.eql('Soy La master QA')
-		})
-
-		})
-	it('Add sticker to a card', ()=>{	
-		addSticker.addStickerToCard(idCard,key,token)
 	})
+	});
+		it('TC02: Validate that a sticker can be added to a card', () =>{
+			addSticker.addStickerToCard(idCard, key, token).then(response =>{
+			expect(response.status).to.eql(200);
+			expect(response.body.image).to.eql('taco-money')
+			expect(response.body.zIndex).to.eql(1)
 
-	}) 
+			})
+		});
+
+		it('TC03:update sticker on card', () =>{
+			updateSticker.updateStickerOnCard(idCard, idSticker, key, token).then(response =>{
+			expect(response.status).to.eql(200);
+			expect(response.body.image).to.eql('taco-money')
+			expect(response.body.left).to.eql(6)
+			expect(response.body.zIndex).to.eql(1)
+			})
+
+		})
+
+		it('TC03: Delete Sticker from Card', () =>{
+
+		})
+
+}) 
+
+
--- cypress/support/pages/APIStickers.page.ts
+++ cypress/support/pages/APIStickers.page.ts
@@ -1,3 +1,5 @@
+import { method } from "cypress/types/bluebird";
+
 export class apiSticker{
 	static createNewCard ( idList: string, key:string,token: string, ){
 		
@@ -14,13 +16,8 @@ export class apiSticker{
 			}
 		}).then(response=>{
 			return response;
-		})
-
-		
+		})		
 	}
-
-
-
 	
 }
 export class addSticker{
@@ -29,7 +26,7 @@ export class addSticker{
 			method: 'POST',
 			url:`https://api.trello.com/1/cards/${idCard}/stickers?key=${key}&token=${token}`,
 				qs:{
-			    image: 'taco-alert',
+			    image:'taco-money',
 				top: 1,
 				left: 6,
 				zIndex: 1, 
@@ -41,4 +38,45 @@ export class addSticker{
 			return response;
 		})
 
-		}}
+}}
+
+export class updateSticker{
+	static updateStickerOnCard(idCard:string, idSticker: string, key: string, token: string){
+		return cy.api({
+			method: 'PUT',
+			url: `https://api.trello.com/1/cards/${idCard}/stickers/${idSticker}?key=${key}&token=${token}`,
+			qs:{
+				image:'taco-money',
+				top: -3,
+				left: 6,
+				zIndex: 1, 
+				rotate: 1,
+				key: key,
+				token: token,
+			}
+		}).then(response =>{
+			return response; 
+		})
+	}
+}
+export class deleteSticker{
+	static deleteStickerCard(idCard: string, idSticker: string, key: string, token: string){
+		return cy.api({
+			method: 'DELETE',
+			url: `https://api.trello.com/1/cards/${idCard}/stickers/${idSticker}?key=${key}&token=${token}`,
+			qs:{
+			image:'taco-money',
+			top: -3,
+			left: 6,
+			zIndex: 1, 
+			rotate: 1,
+			key: key,
+			token: token,
+			}
+
+			
+		})
+
+	
+	}
+}
