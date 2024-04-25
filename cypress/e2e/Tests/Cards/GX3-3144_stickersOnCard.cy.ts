72e0f19fc3f574067f0c4e28b73801449add2c72
ZwitterKaneda <smentek@gmail.com>
2024-04-25 19:31:29 -0300

    GX3-3144 helping Ele

--- cypress/e2e/Tests/Cards/GX3-3144_stickersOnCard.cy.ts
+++ cypress/e2e/Tests/Cards/GX3-3144_stickersOnCard.cy.ts
@@ -1,45 +1,78 @@
-import { apiSticker } from "@pages/APIStickers.page";
-import { addSticker } from "@pages/APIStickers.page";
-import { updateSticker } from "@pages/APIStickers.page";
-import { deleteSticker } from "@pages/APIStickers.page";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { UpdateSticker, DeleteSticker,AddSticker,ApiSticker } from '@pages/APIStickers.page';
+import data from '../../../fixtures/data/GX3-3144_stickersOnCardData.json';
+import {faker}from '@faker-js/faker';
 
-	const key = '60546f7606a6d9b65762f53b68d61aa2'
-	const token= 'ATTAba481ff908481346282087e391a6df7c1cad1c2f00b5baa233765cec3e5b26b26CBAC466'
-	const idList = '66228c917bf9a7fda9f9403d'
-	const idCard = '6622904b2d086ade25bce1a7'
-	const idSticker= '662ad1ca3c2061960b4b5fed'
-      
+const {key} = data;
+const {token} = data;
+const idList = '66228c917bf9a7fda9f9403d';
+const idCard = '6622904b2d086ade25bce1a7';
+const idSticker= '662ad1ca3c2061960b4b5fed';
 
-	describe('API Trello board ',()=>{
-	    it('TC01:create a card in Trello',() => {
-		apiSticker.createNewCard(idList,key,token,).then(response =>{
-			expect(response.status).to.eql(200);
-			expect(response.body.name).to.eql('Soy La master QA')
-	})
-	});
-		it('TC02: Validate that a sticker can be added to a card', () =>{
-			addSticker.addStickerToCard(idCard, key, token).then(response =>{
-			expect(response.status).to.eql(200);
-			expect(response.body.image).to.eql('taco-money')
-			expect(response.body.zIndex).to.eql(1)
 
-			})
+describe('API Trello board ',() => {
+	// before(); //Función que se ejecuta antes de todos los tests
+	// beforeEach(); //Función que se ejecuta antes de cada test
+	// after(); //Función que se ejecuta despues de todos los tests
+	// afterEach(); //Función que se ejecuta despues de cada test
+	beforeEach('Precondition',() => {
+		// conseguir el idBoard con un GET
+		// Realizar el POST de la creación de la List, para guardar el idList
+		// Realizar el POST de la creación de la Card, para guardar el idCard
+		// Crear un sticker, para guardar el idSticker
+		cy.request({
+			method: 'GET',
+			url: `https://api.trello.com/1/members/me/boards?key=${key}&token=${token}`
+		}).then(responseGetBoard => {
+			expect(responseGetBoard.status).to.eql(200);
+			const idBoard = responseGetBoard.body[1].id;
+			const nameList = faker.person.firstName();
+			cy.request({
+				method:'POST',
+				url:`https://api.trello.com/1/lists?name=${nameList}&idBoard=${idBoard}&key=${key}&token=${token}`
+			}).then(responsePostList => {
+				expect(responsePostList.status).to.eql(200);
+				const idList = responsePostList.body.id;
+			});
 		});
 
-		it('TC03:update sticker on card', () =>{
-			updateSticker.updateStickerOnCard(idCard, idSticker, key, token).then(response =>{
+	});
+
+	it('TC01:create a card in Trello',() => {
+		ApiSticker.createNewCard(idList,key,token,).then(response => {
 			expect(response.status).to.eql(200);
-			expect(response.body.image).to.eql('taco-money')
-			expect(response.body.left).to.eql(6)
-			expect(response.body.zIndex).to.eql(1)
-			})
+			expect(response.body.name).to.eql('Soy La master QA');
+		});
+	});
+	it('TC02: Validate that a sticker can be added to a card', () => {
+		AddSticker.addStickerToCard(idCard, key, token).then(response => {
+			expect(response.status).to.eql(200);
+			expect(response.body.image).to.eql('taco-money');
+			expect(response.body.zIndex).to.eql(1);
 
-		})
+		});
+	});
 
-		it.only('TC04: Delete Sticker from Card', () =>{
-			deleteSticker.deleteStickerCard(idCard, idSticker, key,token)}		
+	it('TC03:update sticker on card', () => {
+		UpdateSticker.updateStickerOnCard(idCard, idSticker, key, token).then(response => {
+			expect(response.status).to.eql(200);
+			expect(response.body.image).to.eql('taco-money');
+			expect(response.body.left).to.eql(6);
+			expect(response.body.zIndex).to.eql(1);
+		});
 
-)
-}) 
+	});
+
+	it('TC04: Delete Sticker from Card', () => {
+		DeleteSticker.deleteStickerCard(idCard, idSticker, key,token);}
+	);
+
+	afterEach('Postcondition',() => {
+		// Borrar el sticker
+		// Borrar la card
+		// Borrar la List
+	});
+
+});
 
 
