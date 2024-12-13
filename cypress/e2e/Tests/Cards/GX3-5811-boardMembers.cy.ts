f89b526f9eb852d5f369edfb2818df6a4413bf0c
Diego González Fernández <verogeid@gmail.com>
2024-12-13 12:59:25 +0100

    test: (GX3-5811) Implement OAuth 1.0 Authentication
    - Added OAuth 1.0 authentication for API requests.
    - Replaced `cy.request` with `cy.api` for improved API handling.
    
     On branch test/GX3-5811/board-members
    	modified:   cypress/e2e/Tests/Cards/GX3-5811-boardMembers.cy.ts
    	modified:   cypress/fixtures/data/GX3-5811-boardMembers.json

--- cypress/e2e/Tests/Cards/GX3-5811-boardMembers.cy.ts
+++ cypress/e2e/Tests/Cards/GX3-5811-boardMembers.cy.ts
@@ -1,7 +1,9 @@
+import OAuth from 'oauth-1.0a';
+import crypto from 'crypto';
+
 interface Auth {
   key: string;
   token: string;
-  verificacion: string;
   user: string;
 }
 
@@ -26,6 +28,18 @@ var strConnUser: string;
 var strConnKey: string;
 var strConnToken: string;
 
+// Crea una instancia de OAuth
+const oauth = new OAuth({
+	consumer: {
+		key: 'TU_CONSUMER_KEY', // Reemplaza con tu Consumer Key
+		secret: 'TU_CONSUMER_SECRET', // Reemplaza con tu Consumer Secret
+	},
+	signature_method: 'HMAC-SHA1',
+	hash_function(baseString: string, key: string): string {
+		return crypto.createHmac('sha1', key).update(baseString).digest('base64');
+	},
+});
+
 describe('GX3-5811 | Trello (API) | Members | API Endpoint: Get the Members of a Board', () => {
 	let fixtureData: UserData;
 
@@ -48,15 +62,30 @@ describe('GX3-5811 | Trello (API) | Members | API Endpoint: Get the Members of a
 			.replace('{{myKey}}', strConnKey)
 			.replace('{{myToken}}', strConnToken);
 
-		cy.request({
+		cy.log(URL_MEMBERS_BOARD);
+
+		const requestData = {
+			url: URL_MEMBERS_BOARD,
+			method: 'GET',
+			data: {
+				idBoard: fixtureData.data.idBoard,
+			},
+		};
+
+		const oauthData = oauth.authorize(requestData, {
+			key: strConnKey,
+			secret: strConnToken,
+		});
+
+		// Usando cy.api() en lugar de cy.request()
+		cy.api({
 			method: 'GET',
 			url: URL_MEMBERS_BOARD,
 			headers: {
-				'authorization': `Bearer ${strConnToken}`
+				'Authorization': oauth.toHeader(oauthData)['Authorization'],
 			}
 		}).then((response) => {
 			expect(response.status).to.eq(200);
-
 		});
 	});
 
